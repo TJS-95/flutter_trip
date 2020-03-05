066c85c71ac16114bda8a885bb8a50a66a6eba61
tanjs <tjs_95@163.com>
2020-03-05 21:06:44 +0800

    banner轮播图

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,14 +7,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final PageController _controller = PageController(
-    initialPage: 0,
-  );
+  List _imageUrls = [
+    'http://pic1.win4000.com/wallpaper/c/57b67b4952234.jpg',
+    'http://pic1.win4000.com/wallpaper/1/541288f212ea4.jpg',
+    'http://pic1.win4000.com/wallpaper/2018-11-06/5be15b7635e5d.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                  itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageUrls[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
